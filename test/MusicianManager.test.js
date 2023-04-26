40294e1b8f45462680c959e7ad785781f451a691
Philoem <philoem24@gmail.com>
2023-04-26 21:51:23 +0200

    test: add 2 tests

--- test/MusicianManager.test.js
+++ test/MusicianManager.test.js
@@ -23,4 +23,21 @@ contract('MusicianManager', accounts => {
         const result = await contract.addTrack('0x499e316e8c8dae95096d30b9fef3424c3bd8963c', 'titleTrack', 180, {from: accounts[0]});
         assert.equal(result.logs[0].args._title, 'titleTrack', 'Not equal to title');
     })
+
+    it('Should not add a track to an unknown artist', async () => {
+        const contract = await MusicianManager.deployed();
+        let err = null;
+        try {
+            await contract.addTrack('0xd3320e1bcc80bdad051caa32eccbbbcace4ec90f', 'Track01', 252, {from: accounts[1]})
+        } catch (error) {
+            err = error;
+        }
+        assert.ok(err instanceof Error);
+    })
+
+    it('Should get the tracks of an artist', async () => {
+        const contract = await MusicianManager.deployed();
+        const result = await contract.getTracks('0x499e316e8c8dae95096d30b9fef3424c3bd8963c', {from: accounts[0]});
+        assert.ok(Array.isArray(result.logs[0].args._tracks));
+    })
 })
